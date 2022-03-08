c7a20442f340f7bf2d7de43eecda4a351126f53d
bresar <bremysromero85@gmail.com>
2022-03-08 13:32:19 -0500

    types date TypeScript

--- prueba.ts
+++ prueba.ts
@@ -1,3 +1,22 @@
 let n = 25
+let color: string = "blue"
+color = "red"
 
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+let sentences: string = `Hello, my name is ${fullName}.
+I'll be ${age + 1} years old next month`
+let list: number[] = [1, 2, 3]
+
+let x: [string, number]
+x = ["sara", 24]
+let [cadena, numero] = x
+
+//console.log(typeof(numero))
+console.log(cadena, numero)
+console.log(x)
+console.log(fullName)
+console.log(sentences)
+console.log(color)
 console.log(n)
+console.log(list)
